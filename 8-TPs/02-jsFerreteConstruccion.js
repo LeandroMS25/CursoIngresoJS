69b080f6826bf89827c4dfbae7c091770c9c5a40
LeandroMS25 <leandro.m.sobrino@gmail.com>
2020-07-14 23:31:01 -0300

    Ejercicios realizados en clase 2

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -25,9 +25,42 @@ function Rectangulo ()
 }
 function Circulo () 
 {
-	
+    var radio;
+    var pi; 
+    var circunferencia;
+    var cantidadAlambre;
+
+    radio = txtIdRadio.value;
+    radio = parseInt(radio);
+    
+    pi = 3.14;
+
+    circunferencia = 2 * pi * radio;
+
+    cantidadAlambre = circunferencia * 3;
+
+    cantidadAlambre = parseInt(cantidadAlambre);
+
+    alert("La cantidad de alambre que se debe comprar es: " + cantidadAlambre);
 }
 function Materiales () 
 {
-	
+	var largo;
+    var ancho;
+    var area;
+    var bolsaCemento;
+    var bolsaCal;
+
+    largo = txtIdLargo.value;
+    ancho = txtIdAncho.value;
+
+    largo = parseInt(largo);
+    ancho = parseInt(ancho);
+
+    area = largo * ancho;
+
+    bolsaCemento = area * 2;
+    bolsaCal = area * 3;
+
+    alert("Se necesitan " + bolsaCemento + " bolsas de cemento y " + bolsaCal + " bolsas de cal.")
 }
