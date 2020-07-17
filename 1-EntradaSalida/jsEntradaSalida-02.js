008c328f49c066f1de056780f63e043ad4df6285
LeandroMS25 <leandro.m.sobrino@gmail.com>
2020-07-16 23:17:12 -0300

    Ejercicios realizados en clase 3

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,7 +3,7 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	//La variable debe llevar un nombre representativo del dato que se guarda en haya.
+	//La variable debe llevar un nombre representativo del dato que se guarda en ella.
 	var dato; // Notacion camel case - todas las variables que escribo deben ir en minuscula.
 	dato=prompt("Ingrese un dato."); // Para guardar datos en la variable usar el operador de inicializacion "=".	
 	alert(dato);// Aca termina el ejercicio
